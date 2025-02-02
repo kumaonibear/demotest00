24f7cd74167548b2f9ca9b9eeb7741ef57f98be8
asron <ronasron2546@gmail.com>
2025-02-02 15:45:06 +0700

    add

--- animal/admin.py
+++ animal/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from animal.models import Animal
 
 # Register your models here.
-
+# aspdofka;sofapfokaspofkaepofkaepofkaepofkapeofkaepofk
 
 admin.site.register(Animal)
